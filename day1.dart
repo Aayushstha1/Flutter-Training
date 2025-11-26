78280942804e61e8538cf66283aace2a3c9b20a1
Aayushstha1 <sudipk339@gmail.com>
2025-11-26 08:47:18 +0545

    New

---
+++ day1.dart
@@ -0,0 +1,20 @@
+void main (){
+//   //<datatype > variablename;
+//    int age = 18 ; 
+//   print(age);
+//   print('Your age is $age');
+//   String name = "Hello world";
+//   print(name);
+
+
+
+// var , final , const 
+//   var name = "hello" ; 
+//   print(name);
+  
+  final String name = "Namuna"; 
+  final double doubleValue =  initValue;
+  
+  
+  
+}
