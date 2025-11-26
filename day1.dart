3356691fb5b4f7ce4e76ad76ba805cc8a92a5506
Aayushstha1 <sudipk339@gmail.com>
2025-11-26 08:58:02 +0545

    New

--- day1.dart
+++ day1.dart
@@ -1,20 +1,22 @@
-void main (){
-//   //<datatype > variablename;
-//    int age = 18 ; 
-//   print(age);
-//   print('Your age is $age');
-//   String name = "Hello world";
-//   print(name);
+void main() {
+  //   //<datatype > variablename;
+  //    int age = 18 ;
+  //   print(age);
+  //   print('Your age is $age');
+  //   String name = "Hello world";
+  //   print(name);
 
+  // var , final , const
+  //   var name = "hello" ;
+  //   print(name);
 
+  // final String name = "Namuna";
+  // final double doubleValue =  initValue;
 
-// var , final , const 
-//   var name = "hello" ; 
-//   print(name);
-  
-  final String name = "Namuna"; 
-  final double doubleValue =  initValue;
-  
-  
-  
+  // dynamic name = "Namuna";
+  // name = 20;
+  List<dynamic> names = ["aayush", "ram"];
+  names.add("sam");
+  names[0] = "Stha";
+  print(names);
 }
