eea26cfcfaa217ed77cac8f1897e025c2e2b1ae2
Aayushstha1 <sudipk339@gmail.com>
2025-11-27 09:23:13 +0545

    New

--- day2/flutterday2/lib/main.dart
+++ day2/flutterday2/lib/main.dart
@@ -1,3 +1,28 @@
-void main(){
-  
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //   return MaterialApp(
+    //     home: Scaffold(
+    //       appBar: AppBar(title: Text("helow title")),
+
+    //       body: Text("Hello babe"),
+    //     ),
+    //   );
+    // }
+
+    return MaterialApp(
+      home: Scaffold(body: Text("Hellow dart ", style: TextStyle(
+        fontSize: 24, 
+        color: Colors.blueAccent
+      ))),
+    );
+  }
 }
